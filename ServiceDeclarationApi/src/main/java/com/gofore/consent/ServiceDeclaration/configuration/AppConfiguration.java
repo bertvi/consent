251add148c92fee51ea3ed46176092752ad57191
bertvi <bert.viikmae@gofore.com>
2020-04-05 16:44:24 +0300

    add enable OpenAPI3

--- ServiceDeclarationApi/src/main/java/com/gofore/consent/ServiceDeclaration/configuration/AppConfiguration.java
+++ ServiceDeclarationApi/src/main/java/com/gofore/consent/ServiceDeclaration/configuration/AppConfiguration.java
@@ -2,16 +2,11 @@ package com.gofore.consent.ServiceDeclaration.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
