5d31ed16779b596298385db0cda8acf854c17558
bertvi <bert.viikmae@gofore.com>
2020-04-05 20:22:45 +0300

    add logging

--- ServiceDeclarationApi/src/main/java/com/gofore/consent/ServiceDeclaration/Application.java
+++ ServiceDeclarationApi/src/main/java/com/gofore/consent/ServiceDeclaration/Application.java
@@ -1,5 +1,7 @@
 package com.gofore.consent.ServiceDeclaration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +10,13 @@ import java.util.Collections;
 @SpringBootApplication
 public class Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Application.class);
 		app.setDefaultProperties(Collections.singletonMap("server.port", "8982"));
 		app.run(args);
+		log.info("Service Declaration API started on port 8982");
 	}
 
 }
--- ServiceDeclarationApi/src/main/java/com/gofore/consent/ServiceDeclaration/ServiceDeclarationApiService.java
+++ ServiceDeclarationApi/src/main/java/com/gofore/consent/ServiceDeclaration/ServiceDeclarationApiService.java
@@ -7,14 +7,18 @@ import com.gofore.consent.ServiceDeclaration.model.*;
 import com.gofore.consent.ServiceDeclaration.repository.ServiceDeclarationRepository;
 import com.gofore.consent.ServiceDeclaration.repository.ServiceProviderRepository;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class ServiceDeclarationApiService {
+
+    private static final Logger log = LoggerFactory.getLogger(ServiceDeclarationApiService.class);
 
     @Autowired
     private ServiceProviderRepository serviceProviderRepository;
@@ -33,10 +37,14 @@ public class ServiceDeclarationApiService {
     public List<ServiceDeclaration> findDeclarations(ListServiceDeclarationRequest request) throws InvalidRequestException, TooBroadQueryException {
         List<ServiceDeclaration> declarations;
         ServiceProvider provider;
+
+        log.info("Attempting to find declarations with request {}", request.toString());
+
         if (providerExists(request.getServiceProviderIdentifier())) {
             provider = findProviderByIdentifier(request.getServiceProviderIdentifier());
             declarations = serviceDeclarationRepository.findByProvider(provider);
         } else {
+            log.error("The provider with identifier {} does not exist", request.getServiceProviderIdentifier());
             throw new InvalidRequestException("The provider with identifier " + request.getServiceProviderIdentifier() + " does not exist");
         }
 
@@ -45,8 +53,11 @@ public class ServiceDeclarationApiService {
                 && request.getName().isEmpty();
 
         if (tooBroadQuery) {
+            log.error("Consent Service refuses to fulfill the request/too many responses, please add more specific conditions and try again");
             throw new TooBroadQueryException("Consent Service refuses to fulfill the request/too many responses, please add more specific conditions and try again");
         }
+
+        log.info("Found following declarations: {}", declarations.toString());
 
         return declarations;
     }
@@ -63,6 +74,9 @@ public class ServiceDeclarationApiService {
 
     public ServiceDeclaration save(AddServiceDeclarationRequest request) throws DuplicateDeclarationException, InvalidRequestException {
         ServiceProvider provider;
+
+        log.info("Attempting to save declaration with request {}", request.toString());
+
         if (providerExists(request.getServiceProviderIdentifier())) {
             provider = findProviderByIdentifier(request.getServiceProviderIdentifier());
         } else {
@@ -72,15 +86,18 @@ public class ServiceDeclarationApiService {
         }
 
         if (declarationExists(request.getServiceDeclarationIdentifier())) {
+            log.error("There already exists a declaration with identifier: {}", request.getServiceDeclarationIdentifier());
             throw new DuplicateDeclarationException("There already exists a declaration with identifier: " +
                     request.getServiceDeclarationIdentifier());
         }
 
         if (request.getValidUntil().isBefore(LocalDateTime.now())) {
+            log.error("The validUntil of this declaration request is in the past");
             throw new InvalidRequestException("The validUntil of this declaration request is in the past");
         }
 
         if (request.getMaxCacheSeconds().longValue() < 0) {
+            log.error("MaxCacheSeconds of service declaration must be positive");
             throw new InvalidRequestException("MaxCacheSeconds of service declaration must be positive");
         }
 
@@ -91,19 +108,28 @@ public class ServiceDeclarationApiService {
         declaration.setValid(Boolean.TRUE);
         declaration.setProvider(provider);
 
+        log.info("Saving declaration {} to db", declaration.toString());
+
         return serviceDeclarationRepository.save(declaration);
     }
 
     public ServiceDeclaration update(UpdateServiceDeclarationRequest request) throws InvalidRequestException {
+        log.info("Attempting to update declaration with request {}", request.toString());
+
         if (!declarationExists(request.getServiceDeclarationIdentifier())) {
+            log.error("The declaration with identifier {} does not exist", request.getServiceDeclarationIdentifier());
             throw new InvalidRequestException("The declaration with identifier " + request.getServiceDeclarationIdentifier() + " does not exist");
         }
         if (request.getValidUntil().isBefore(LocalDateTime.now())) {
+            log.error("The validUntil of this declaration request is in the past");
             throw new InvalidRequestException("The validUntil of this declaration request is in the past");
         }
 
         ServiceDeclaration declaration = serviceDeclarationRepository.findByIdentifier(request.getServiceDeclarationIdentifier());
         declaration.setValid(Boolean.TRUE);
+
+        log.info("Updating declaration {} in db", declaration.toString());
+
         return serviceDeclarationRepository.save(declaration);
     }
 
